37db5588e5bd7a6cf1be1e71010d6d9636db17bf
kostelnik.j <kostelnik.j@seznam.cz>
2018-12-17 13:50:01 +0100

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("Hodnota je " + x);
+
+            double dnum = 0.1d;
+            float fnum = 0.2f;
+
+        }
+    }
+}
