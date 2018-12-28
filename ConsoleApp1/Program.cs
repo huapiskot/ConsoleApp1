aa43b4056d503bec258267209ee48e7f29260788
kostelnik.j <kostelnik.j@seznam.cz>
2018-12-28 12:17:35 +0100

    x

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,18 +17,18 @@ namespace ConsoleApp1
             float fnum = 0.2f;
             decimal decnum = 5.5m;
 
-            Console.WriteLine("Decimal: {0}, double: {1}, float: {2}",decnum,dnum,fnum);
+            Console.WriteLine("Decimal: {0}, double: {1}, float: {2}", decnum, dnum, fnum);
 
             char c = 'c';
             string jmeno = "John";
 
             bool isDone = false;
 
-            Console.WriteLine(""+dnum+fnum+decnum+c+jmeno+isDone);
+            Console.WriteLine("" + dnum + fnum + decnum + c + jmeno + isDone);
 
             int cislo = 15;
             string strCislo = cislo.ToString();
-            float ff =3.5f;
+            float ff = 3.5f;
             cislo = Convert.ToInt32(ff); //4
             cislo = (int)ff; //3
             Console.WriteLine(cislo);
@@ -38,7 +38,37 @@ namespace ConsoleApp1
 
             Console.WriteLine(typeof(int).IsPrimitive);
 
+            DateTime dt = new DateTime(); //01.01.0001 0:00:00
+            Console.WriteLine(dt);
 
+            dt = new DateTime(2018, 12, 20, 13, 15, 20, 20); //20.12.2018 13:15:20:20
+            Console.WriteLine(dt);
+
+            dt = DateTime.Now; //20.12.2018 11:16:42
+            Console.WriteLine(dt);
+            Console.WriteLine();
+
+            Console.WriteLine(dt.ToShortDateString());  //20.12.2018
+            Console.WriteLine(dt.ToShortTimeString());  //11:19
+            Console.WriteLine(dt.ToLongDateString());   //čtvrtek 20. prosince 2018
+            Console.WriteLine(dt.ToLongTimeString());   //11:19:12
+
+            Console.WriteLine(dt.AddDays(11));
+
+            string formatedDate = string.Format("Datum je {0:yyyy MM dd: HH mm ss}", dt); //Datum je 2018 12 20: 11 37 22
+            string formatedDate2 = string.Format("Datum je {0:yyyy MMMM ddd: HH mm ss}", dt); //Datum je 2018 prosinec čt: 11 39 42
+            string s = dt.ToString("yyyy-MM-dd HH:mm:ss"); //2018-12-20 11:46:40
+
+
+            Console.WriteLine(string.Format("Datum je {0:yyyy MMMM 'nejaky text yyyy uprostred' ddd: HH mm ss}", dt));
+
+            ushort sh = 125;
+            switch (sh)
+            {
+                case 1: Console.WriteLine("xx"); break;
+                case 0: Console.WriteLine("err!"); break;
+                default: Console.WriteLine("aaaa"); break;
+            }
         }
     }
 }
